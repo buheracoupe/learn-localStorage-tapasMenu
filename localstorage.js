56c6029a0e3c065ba8d80b599a5efe17ddb905d1
Darrell <176396718+buheracoupe@users.noreply.github.com>
2024-09-04 09:59:43 +0200

    add clear and delete buttons to change state of the form and update localstorage

--- localstorage.js
+++ localstorage.js
@@ -2,6 +2,8 @@ const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
   const itemInput = document.querySelector('[name=item]')//selecting the input with attribute won't work, figure out why
   const items = JSON.parse(localStorage.getItem("items")) || [];
+  const deleteBtn = document.getElementById("delete-btn");
+  const clearBtn = document.getElementById("clear-btn");
 
   
 
@@ -32,13 +34,25 @@ list.innerHTML = array.map((item, i) => {
     if(!event.target.matches("input")) return;//not inequality did not work here investigate why?
     console.log(event.srcElement.dataset.index)
     const index = event.srcElement.dataset.index;
-    items[index].done = !items.[index].done;
+    items[index].done = !items[index].done;
     localStorage.setItem("items", JSON.stringify(items))
     populateList(items, itemsList)
     // line 35-37 change our property, update that to local storage and update rendered content
   }
+function deleteList(event){
+items.splice(0, items.length)
+populateList(items, itemsList)
+}
+function clearChecks(){
+items.forEach(item => {
+    item.done = false;
+});
+populateList(items,itemsList)
+}
 
   addItems.addEventListener("submit", handleSubmission)
 populateList(items, itemsList)//populate the list from localStorage if items already exist
 itemsList.addEventListener("click", toggleDone)
-  //remember that each item is an object and these object have text properties for line label element in populateList
+deleteBtn.addEventListener("click", deleteList)
+clearBtn.addEventListener("click", clearChecks)
+  //remember that each item is an object and these object have text properties for line label element in populateListg
